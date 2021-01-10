38299a7558331bea293c18db2b9b3aece5bb20e2
Aidan <aidanbarber88@hotmail.com>
2021-01-10 14:11:07 -0500

    barcode

--- application-interface/src/App.js
+++ application-interface/src/App.js
@@ -1,38 +1,15 @@
-import React from 'react'
-//import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-import {Button, Alert, Breadcrumb, Card} from 'react-bootstrap'
-
+import React from 'react';
+import { useBarcode } from 'react-barcodes';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Card>
-          <Card.Img/>
-          <Card.Body>
-            <Card.Title>
-              Card Example
-            </Card.Title>
-            <Card.Text>
-              Example of Card
-            </Card.Text>
-            <Button varriant="primary">read more</Button>
-          </Card.Body>
-        </Card>
-        <Breadcrumb>
-        <Breadcrumb.Item>Test</Breadcrumb.Item>
-        <Breadcrumb.Item>Test 2</Breadcrumb.Item>
-        <Breadcrumb.Item active> Test 3</Breadcrumb.Item>
-        </Breadcrumb>
-        <Alert variant = "success">This is a button</Alert>
-        <Button>Test Button</Button>
-
-      </header>
-    </div>
-  );
-}
+  const { inputRef } = useBarcode({
+    value: 'react-barcodes',
+    options: {
+      background: '#ccffff',
+    }
+  });
+  
+  return <svg ref={inputRef} />;
+};
 
 export default App;

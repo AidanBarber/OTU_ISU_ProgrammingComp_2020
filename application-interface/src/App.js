8934f7751a1d3c596781b22ca148ff183f426f7f
Aidan <aidanbarber88@hotmail.com>
2021-01-10 11:49:51 -0500

    testing bootstrap

--- application-interface/src/App.js
+++ application-interface/src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
+import React from 'react'
+//import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import {Button, Alert, Breadcrumb, Card} from 'react-bootstrap'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card>
+          <Card.Img/>
+          <Card.Body>
+            <Card.Title>
+              Card Example
+            </Card.Title>
+            <Card.Text>
+              Example of Card
+            </Card.Text>
+            <Button varriant="primary">read more</Button>
+          </Card.Body>
+        </Card>
+        <Breadcrumb>
+        <Breadcrumb.Item>Test</Breadcrumb.Item>
+        <Breadcrumb.Item>Test 2</Breadcrumb.Item>
+        <Breadcrumb.Item active> Test 3</Breadcrumb.Item>
+        </Breadcrumb>
+        <Alert variant = "success">This is a button</Alert>
+        <Button>Test Button</Button>
+
       </header>
     </div>
   );
